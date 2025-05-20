64ca1e796f9cd11614e3e2356ae148819b3c450b
eraybedir <65161225+eraybedir@users.noreply.github.com>
2025-05-20 20:01:49 +0300

    Update ApplicationUser.cs

--- backend/MarketAPI.Models/ApplicationUser.cs
+++ backend/MarketAPI.Models/ApplicationUser.cs
@@ -1,44 +1,10 @@
-﻿namespace MarketAPI.Models
+using Microsoft.AspNetCore.Identity;
+
+namespace MarketAPI.Models
 {
-    public enum UserGoal
+    public class ApplicationUser : IdentityUser
     {
-        gainingWeight = 1,
-        doingSports = 2,
-        loosingWeight = 3,
-        beinghealthy = 4
-    }
-
-    public enum ActivityLevel
-    {
-        Sedentary,         // Çok az aktif
-        LightlyActive,
-        ModeratelyActive,
-        VeryActive,
-        ExtraActive
-    }
-
-    public enum Gender
-    {
-        Male,
-        Female
-    }
-
-    public class ApplicationUser
-    {
-        public int Id { get; set; }
-
-        public string FirebaseUid { get; set; } // Firebase Authentication UID
-        public string Email { get; set; }
-
-        public int Age { get; set; }
-        public Gender Gender {  get; set; }
-
-        public double Weight { get; set; }
-        public double Height { get; set; }
-
-        public UserGoal Goal { get; set; }
-        public ActivityLevel ActivityLevel { get; set; }
-
-        public double Budget { get; set; }
+        public required string FirebaseUid { get; set; }
+        public required string Email { get; set; }
     }
 }
